3f03daf5aa32bba65710be31a6e957998fb67a83
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-10-13 00:04:41 +0300

    added docstring

--- quiz_service/api/serializers.py
+++ quiz_service/api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer
-
 from .models import QuizQuestion
 
 
 class QuizQuestionsSerializer(ModelSerializer):
-    """Сериализатор для модели QuizQuestion."""
+    """
+    Сериализатор для модели QuizQuestion.
+
+    Args:
+    - question_id (int): Уникальный идентификатор вопроса.
+    - question_text (str): Текст вопроса для викторины.
+    - answer_text (str): Текст ответа на вопрос.
+    - created_at (str): Дата и время создания записи в базе данных.
+    """
 
     class Meta:
         model = QuizQuestion
